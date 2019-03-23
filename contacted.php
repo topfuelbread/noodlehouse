a12fac105cf56d7bf1b2a68efadd5e1966368f18
topfuelbread <40782646+topfuelbread@users.noreply.github.com>
2019-03-23 17:47:38 -0400

    Add files via upload
    Clicking the submit button will send an email to the site owner

--- contacted.php
+++ contacted.php
@@ -1,16 +1,13 @@
 
-<!DOCTYPE html>
 <html>
 	<head>
 		<title> Web Dev Noodle House </title>
 		<link rel="stylesheet" type="text/css" href="style.css"/>
         <link href="https://fonts.googleapis.com/css?family=Cardo:400,700|Oswald" rel="stylesheet">
-		<script src="js/tinymce/tinymce.min.js"></script>
-		<script> tinymce.init({selector: 'textarea'}); </script>
 	</head>
-	
 	<body>
-	<div class="container-left">
+        
+		<div class="container-left">
             <br>
             <div class="title">Noodle House</div>
             <div class="aside-block login">
@@ -25,14 +22,14 @@
                             <input id="password" type="password" name="password"/>
                         </p>
                         <button type = "submit">Login</button>
-                        <a class="button" href="/createaccount">Create Account</a>
+                        <a class="button" href="createaccount">Create Account</a>
                     </form>
             </div>
             <br>
             <div class="menu">
                 <nav>
                     <ul class="page-nav">
-                        <li><a href="/">Home</a></li>
+                        <li><a href="index.html">Home</a></li>
                         <li><a href="menu.php">Menu</a></li>
                         <li><a href="aboutus.php">About Us</a></li>
                         <li><a href="contactus.html">Contact Us</a></li>
@@ -40,31 +37,19 @@
                     </ul>
                 </nav>
             </div>
+            
         </div>
-			</nav>
-			<div class="container-right">
-				<main>
-				<h1> Contact Us</h1>
-					<div class="tq">
-					  <form action="contacted.php" method="POST">
-						<fieldset>
-						  <legend>Contact Us</legenD>
-						  <p class="ques">Email Address</p>
-						  <input type="hidden" name="var" value="value" />
-						  <input type="text" name="email" placeholder="email address"></input><br />
-						  
-						  <p class="ques">Message</p>
-						  <input type="hidden" name="var" value="value" />
-						  <textarea name="message" rows="4" cols="20" placeholder="message"></textarea><br />
-						  <input type="submit" name="Button1" value="Submit" />
-						  <input type="reset" value="Reset" />
-						</fieldset>
-					  </form>
-					  
-					
-					</div>
-				</main>
-			</div>
+        
+        <div class="container-right">
+			<main>
+				<br>The message has been received.</br>
+				<br>We will get back to you shortly.</br>
+				<?php 
+					$to = "kim13d@uwindsor.ca";
+					$subject = "Noodle House";
+					mail($to, $subject, $_POST["message"], $_POST["email"]);
+				?>
+			</main>
 		</div>
 	</body>
 </html>
