d0b0c1aca8cfe9491accd500fb46b5b56bac7a4b
Denis Nadarevic <32303056+bigboydeku@users.noreply.github.com>
2019-03-26 15:30:25 -0400

    Add files via upload
    updated the main page (index) with more responsive and flexible css/html

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 //create a session for all the pages
 	session_start();
@@ -14,81 +14,54 @@
         <link href="https://fonts.googleapis.com/css?family=Cardo:400,700|Oswald" rel="stylesheet">
 	</head>
 	<body>
-        
-		<div class="container-left">
-            <br>
-            <div class="title">Noodle House</div>
-            <div class="aside-block login">
-			
-			<?php
-				
-					if(isset($_SESSION['userid'])){
-			
-						echo '<form action="includes/logout.inc.php" method="post">
 
+		<div class="container-left">
+			<br>
+			<div class="title">Noodle House</div>
+			<div class="aside-block login">
+				<?php
+					if(isset($_SESSION['userid'])){
+						echo '<form action="includes/logout.inc.php" method="post">
 							<button type="submit" name="logout-submit">Logout</button>
-				
 							</form>
-							
 							<div class="menu">
 					<nav>
-					
 						<ul class="page-nav">
                         <li><a href="index.php">Home</a></li>
                         <li><a href="menu.php">Menu</a></li>
                         <li><a href="aboutus.php">About Us</a></li>
                         <li><a href="contactus.php">Contact Us</a></li>
-                        
                     </ul>
                 </nav>
             </div>';
-			
-			
-					}else{
-			
+					} else {
 						echo '<form class="form-login" action="includes/login.inc.php" method="post">
-							
 								<h2>Login</h2>
-					
 							<p>
 								<label for="username">Username</label>
-					
 								<input type="text" name="mailuid" placeholder="Username/E-mail...">
 							</p>
-							
 							<p>
 								<label for="password">Password</label>
-					
 								<input type="password" name="pwd" placeholder="Password...">
 							</p>
-							
 						<button type="submit" name="login-submit">Login</button>
-					
 						</form>
-						
-						
 						<div class="menu">
                 <nav>
                     <ul class="page-nav">
                         <li><a href="index.php">Home</a></li>
-                    
-                        
                     </ul>
                 </nav>
             </div>';
-			
 					}
-				
 				?>
-			
-					<a class="button" href="signup.php">Signup</a>
-				
+				<a class="button" href="signup.php">Signup</a>
+
             </div>
-            </br>
-            
-        </div>
-        
-        <div class="container-right">
+					</br>
+		</div>
+		<div class="container-right">
                 <div class="midsection-picture">
                     <img class="myslide" src="http://runawayrice.com/wp-content/uploads/2017/11/Vietnamese-Beef-Noodle-Soup-Pho-Bo-Recipe.jpg">
                     <img class="myslide" src="https://cms.splendidtable.org/sites/default/files/styles/w2000/public/Pho-Cookbook_Seafood-Pho-LEDE_0.jpg?itok=n5h7VTZG">
@@ -104,10 +77,10 @@
                     for (i = 0; i < x.length; i++) {
                         x[i].style.display = "none";
                     }
-                    
+
                     ind++;
-                    if (ind > x.length) {ind = 1}    
-                    x[ind-1].style.display = "block";  
+                    if (ind > x.length) {ind = 1}
+                    x[ind-1].style.display = "block";
                     setTimeout(slideshow, 4000);
                 }
             </script>
@@ -115,33 +88,16 @@
 
 					<main>
 					<article>
-
-			<?php
-			
-			
+		<?php
 			if(isset($_SESSION['userid'])){
-				
-				echo '<h1> 
-						Welcome to Noodle House, since you are logined in, you get access to our members only menu ! ! ! 
-					</h1>
-					<p> 
-					The number one spot to order a taste of Vietnam\'s heavenly meals Try out our famous pho noodle soups or hop on in to get a freshly made salads! 
-					
-					';
-				
+
+				echo '<h1>Welcome to Noodle House, since you are logined in, you get access to our members only menu ! ! !</h1>
+					<p>The #1 spot to order our heavenly meals. Try out our famous pho noodle soups or hop on in to get our freshly made salads!';
+
 			}else{
-				
-				
-				echo'<h1>Sorry, this is a members only restaurant, in order to get access to our website, please login if you are a member, otherwise simply click Signup.</h1>';
-				
-				
+				echo'<h4>Sorry, this is a members only restaurant, in order to get access to our website, please login if you are a member, otherwise simply click Signup.</h1>';
 			}
-		
-			
-			?>
-            
-				
-						
+		?>
 					</article>
 				</main>
 			</div>
